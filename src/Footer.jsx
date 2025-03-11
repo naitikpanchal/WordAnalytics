fc17f6f3654a3c48bf20fc03e4719ec9d10ec701
Naitik Panchal <naitikpanchal25@gmail.com>
2025-03-11 00:55:28 -0400

    added textarea css

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <small>&copy;2025. Copyright by Naitik. All rights reserved.</small>
+            <small>Last checked limits 1 minute ago</small>
+        </footer>
+    )
+}
---
+++ src/TextArea.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
+export default function TextArea() {
+
+    const [text, setText] = useState('');
+    const [warningText, setWarningText] = useState('');
+
+    const handleChange = (e) => {
+        let newText = e.target.value;
+        if ( newText.includes('<script') || newText.includes('</script>') ) {
+            setWarningText('No script tags allowed!');
+            newText = newText.replace(/<script/g, '');
+            newText = newText.replace(/<\/script>/g, '');
+        }
+        else
+            setWarningText('');
+        if ( newText.includes('@') ) {
+            setWarningText('No "@" symbol allowed!');
+            newText = newText.replace(/@/g, '');
+        }
+        else
+            setWarningText('');
+
+        setText(newText);
+    }
+
+    return (
+        <div className="textarea">
+            <textarea value={text} onChange={handleChange} placeholder="Enter your text..." spellCheck="false" />
+            {warningText ? <Warning warningText={warningText}/> : null}
+        </div>
+    )
+}
